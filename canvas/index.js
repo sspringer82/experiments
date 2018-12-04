c5b99b7b2b74475c1cc0654e457092f6f8071e3e
Sebastian Springer <info@sebastian-springer.com>
2018-12-04 20:26:19 +0100

    activate

--- canvas/index.js
+++ canvas/index.js
@@ -10,6 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
     matrix[x][y] = new Dot(ctx, x + 1, y + 1);
   });
   matrixWalker(matrix, (x, y, dot) => dot.render());
+
+  canvas.addEventListener('click', e => {
+    matrixWalker(matrix, (x, y, dot) => {
+      if (dot.isClicked(e.offsetX, e.offsetY)) {
+        dot.toggle();
+      }
+    });
+  });
 });
 
 function matrixWalker(matrix, cb) {
@@ -25,12 +33,29 @@ class Dot {
     this.spaceVertical = 30;
     this.x = x * this.spaceHorizontal;
     this.y = y * this.spaceVertical;
-    this.size = 2;
+    this.size = 4;
+    this.active = false;
   }
 
   render() {
     this.ctx.beginPath();
     this.ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, true);
     this.ctx.fill();
+  }
+
+  isClicked(x, y) {
+    return Math.sqrt((x - this.x) ** 2 + (y - this.y) ** 2) < this.size;
+  }
+
+  toggle() {
+    if (this.active) {
+      this.size = 4;
+      this.ctx.fillStyle = 'black';
+    } else {
+      this.size = 8;
+      this.ctx.fillStyle = 'red';
+    }
+    this.active = !this.active;
+    this.render();
   }
 }
