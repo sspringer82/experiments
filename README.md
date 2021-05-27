b4af86119bde9b979fc5ad6248e017c98be34ee4
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-05-27 09:22:03 +0200

    cleanup

--- README.md
+++
@@ -1,4 +0,0 @@
-# WAT?
-
-This is a repo just for playing around and keeping some of my experiments
-organised.
--- ngDnD/src/app/ng-draggable.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NgDraggableDirective } from './ng-draggable.directive';
-
-describe('NgDraggableDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NgDraggableDirective();
-    expect(directive).toBeTruthy();
-  });
-});
